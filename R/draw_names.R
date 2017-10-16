51e32d31e27951c870e31acafffd36c415478bba
Lavin9 <bw0123@126.com>
2017-10-16 16:21:46 +0200

    2nd version

--- R/draw_names.R
+++ R/draw_names.R
@@ -1,12 +1,12 @@
 #' To plot simultaneously the popularity of more than one name
 #'
 #' @param names names from input 
-#' @import prenoms assertthat dygraphs dplyr
+#' @import prenoms assertthat dygraphs dplyr tidyr
 #' @return the plot 
 #' @export
 
 draw_names <- function(names){
-  assert_that(is.vector(names,mode=character()))
+  assert_that(is.vector(names))
   prenoms::prenoms %>%
     filter(name %in% names) %>%
     group_by(name,year) %>%
--- R/save_as_csv.R
+++ R/save_as_csv.R
@@ -4,13 +4,13 @@
 #' @param filename the name of csv 
 #' @param row.names to suppress row names 
 #' @param ... others 
+#' @import assertthat 
 #' @export
 
 save_as_csv <- function(dataset, filename, row.names = FALSE, ...){
   filename <- normalizePath(filename)
   assert_that(has_extension(filename, "csv"))
   assert_that(is.dir(dirname(filename)))
-  assert_that(is.writeable(dirname(filename)))
   assert_that(not_empty(dataset))
   assert_that(is.data.frame(dataset))
   write.csv2(x=dataset, file = filename)
